2c7b67aeab82708d38c59314881f41fc253ef65c
Paras Dhiman <55368020+dhimanparas20@users.noreply.github.com>
2024-10-22 21:53:30 +0530

    Update test.py

--- others/test.py
+++ others/test.py
@@ -3,7 +3,7 @@ import os
 
 os.system("clear")
 # Create an instance of the LLM, using the 'gemini-pro' model with a specified creativity level
-llm = ChatGoogleGenerativeAI(model='gemini-pro', temperature=0.9, google_api_key="AIzaSyCFZhRAG5iELlbDdqOLKU2ggSk_AsOqjTQ")
+llm = ChatGoogleGenerativeAI(model='gemini-pro', temperature=0.9,
 
 print("Welcome! You can start chatting with the model. Type 'exit' to quit.\n")
 
